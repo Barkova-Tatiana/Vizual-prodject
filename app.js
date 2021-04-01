9d2dddc6c703c7cf3af47f4b2c0a1483ed9f8e08
Barkova-Tatiana <81628902+Barkova-Tatiana@users.noreply.github.com>
2021-04-01 19:56:42 +0300

    add Smooth scroll

--- app.js
+++ app.js
@@ -27,4 +27,21 @@ $(function() {
     }
 
 
+
+    
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 700);
+    });
+
+    
 });
